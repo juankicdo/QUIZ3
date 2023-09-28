212c3524b86560287e226deac3206eed59340d41
juankicdo <juandavidcaicedo1707@gmail.com>
2023-09-27 23:27:42 -0500

    moto

---
+++ Moto.java
@@ -0,0 +1,22 @@
+public class Moto {
+    String marca;
+    double velocidd;
+    double turbo;
+
+    public Moto(String marca, double velocidd, double turbo){
+        this.marca = marca;
+        this.velocidd= velocidd;
+        this.turbo=turbo;
+
+    }
+    void velocidad(){
+        System.out.println("soy re rapido");
+    }
+    void marca(){
+        System.out.println("Honda");
+    }
+    void turbo(){
+        System.out.println("nospi");
+
+    }
+}
---
+++ Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+    public static void main(String[] args) {
+        Moto mimoto = new Moto("Honda", 220, 1.6);
+        mimoto.turbo();
+        mimoto.velocidad();
+        mimoto.marca();
+    }}
